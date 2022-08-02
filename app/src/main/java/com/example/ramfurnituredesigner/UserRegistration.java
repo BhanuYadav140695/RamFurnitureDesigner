bd8f09bbacf7fffe96cbe91097e6123d03deba78
Yadav Bhanu Pratap Vishwanath <94745306+BhanuYadav140695@users.noreply.github.com>
2022-08-02 23:48:28 +0530

    Version 0.0.1

--- app/src/main/java/com/example/ramfurnituredesigner/UserRegistration.java
+++ app/src/main/java/com/example/ramfurnituredesigner/UserRegistration.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class UserRegistration extends AppCompatActivity {
+
+    EditText personName , EmailId , PhoneNumber , PersonAddress , PersonPassword;
+    ImageView PersonImage;
+    ImageButton UploadPersonImage;
+    Button RegisterUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,19 @@ public class UserRegistration extends AppCompatActivity {
         setContentView(R.layout.activity_user_registration);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Loock Screen Rotaion
         getSupportActionBar().hide(); // Hiding Action Bar
+
+        personName = findViewById(R.id.PersonName);
+        EmailId = findViewById(R.id.EmailAddress);
+        PhoneNumber = findViewById(R.id.editTextPhone);
+        PersonAddress = findViewById(R.id.editTextTextPostalAddress);
+        PersonPassword = findViewById(R.id.editTextTextPassword);
+        PersonImage = findViewById(R.id.UserPhoto);
+        UploadPersonImage = findViewById(R.id.imageButton);
+        RegisterUser = findViewById(R.id.SignIn);
+
+
+
+
+
     }
 }
